659c4d59b1581418a2358f878629bfd3227a4aab
Brandon Holman <112407387+bholmdev@users.noreply.github.com>
2024-09-09 15:09:29 -0500

    looping

---
+++ looping.py
@@ -0,0 +1,16 @@
+#!/bin/python3
+
+vegetables = ["cucumber", "spinach", "cabbage"]
+
+for x in vegetables:
+    print(x)
+
+# ip = 1...254
+# for x in ip:
+    # print(x)
+
+i = 1
+
+while i <= 10:
+    print(i)
+    i += 1
