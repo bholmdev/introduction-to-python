9972be2dcef0b60eeb4c3aaef486b1fff1878cc7
fredobuggins <fredobuggins@lab>
2024-09-09 11:58:03 -0500

    stings

---
+++ strings.py
@@ -0,0 +1,9 @@
+#!/bin/python3
+
+# Print string
+print("Hello, world!")
+print('Hello, world!')
+print("""This string runs
+multiple lines!""") # triple quote for multi-line
+print("This string is " + "awesome!") # We can also concatenate
+print("This will go to another\nline") # New line \n
