567b2fadbeb3b54c68615772c9a0c6608d6ef527
Brandon Holman <112407387+bholmdev@users.noreply.github.com>
2024-09-09 14:40:27 -0500

    tuples

---
+++ tuples.py
@@ -0,0 +1,10 @@
+#!/bin/python3
+
+# TUPLES do not change and use ()
+# Immutable
+
+grades = ("a", "b", "c", "d", "f")
+print(grades)
+print(grades[1])
+
+# Methods like .append() and .pop() will not work
