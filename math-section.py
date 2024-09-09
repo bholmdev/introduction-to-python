5e32a948029287c41d697459984567687120c260
bholmdev <112407387+bholmdev@users.noreply.github.com>
2024-09-09 12:51:42 -0500

    math

---
+++ math-section.py
@@ -0,0 +1,11 @@
+#!/bin/python
+
+print(50 + 50) # add
+print(50 - 50) # subtract
+print(50 * 50) # multiply
+print(50 / 50) # divide
+print(50 + 50 - 50 * 50 / 50) # pemdas
+print(50 ** 2) # exponents
+print(50 % 6) # modulo
+print(50 / 6) # division with a remainder
+print(50 // 6) # no remainder
